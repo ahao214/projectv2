5a4ebd12901002c86c4d7bf89f3fa81827157354
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-02 17:22:47 +0800

    表格数据方法封装

--- src/components/students/WorkList.vue
+++ src/components/students/WorkList.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        作业列表
-    </div>
+  <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="id" label="用户ID" align="center">
+      </el-table-column>
+      <el-table-column prop="userId" label="所属班级" align="center">
+      </el-table-column>
+      <el-table-column prop="title" label="作业名称" align="center">
+      </el-table-column>
+      <el-table-column prop="completed_text" label="完成情况" align="center">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
+
+
+<script>
+import {getTableData} from  '@/utils/table.js'
+
+export default{
+    data(){
+        return {
+            tableData:[]
+        }
+    },
+    created(){
+        getTableData(this,'/works',{},['completed'])
+    }
+}
+
+</script>
