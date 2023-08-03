428f89f1b6af0e25e3c8cd308eb6034816e8bfc5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-08-03 21:25:15 +0800

    树形控件

--- src/components/dataAnalysis/ScoreMap.vue
+++ src/components/dataAnalysis/ScoreMap.vue
@@ -1,36 +1,31 @@
 <template>
-    <div class="score-map">
-        <div class="main">
-
-        </div>
-    </div>
+  <div class="score-map">
+    <div class="main"></div>
+  </div>
 </template>
 
 <script>
-import chinaJson  from 'echarts/map/json/china'
+import chinaJson from "echarts/map/json/china";
 
 export default {
-    data(){
-        return {}
-    },
-    mounted(){
-        let myChart = this.$echarts.init(document.getElementById('main'))
-        this.$echarts.registerMap('china',chinaJson)
-        console.log(myChart)
-        // 从这里开始复制实例的代码
-        
-    }
-}
-
-
+  data() {
+    return {};
+  },
+  mounted() {
+    let myChart = this.$echarts.init(document.getElementById("main"));
+    this.$echarts.registerMap("china", chinaJson);
+    console.log(myChart);
+    // 从这里开始复制实例的代码
+  },
+};
 </script>
 
 <style lang="scss">
-.score-ma{
-    width:100%;
-    #main{
-        width:100%;
-        height:500px;
-    }
+.score-ma {
+  width: 100%;
+  #main {
+    width: 100%;
+    height: 500px;
+  }
 }
 </style>
--- src/components/users/UserPage.vue
+++ src/components/users/UserPage.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        UserPage
-    </div>
+  <div>
+    <el-tree :data="menus" show-checkbox node-key="name" :props="defaultProps">
+    </el-tree>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+        label: "name",
+        children: "children",
+      },
+    };
+  },
+  created() {
+    this.menus = [...this.$router.options.routes];
+  },
+};
+</script>
+
+<style lang="scss"></style>
